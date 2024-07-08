09c4e67349791d5dbb770adaee11240be9d3fafb
Jeffrey Hu <hujh14@gmail.com>
2024-07-07 18:03:14 -0700

    multiply normals by alpha

--- examples/benchmark_mcmc.sh
+++ examples/benchmark_mcmc.sh
@@ -37,8 +37,8 @@ do
         --data_dir data/360_v2/$SCENE/ \
         --normal_consistency_loss \
         --dist_loss \
-        --dist_lambda 10 \
-        --result_dir results/3dgs_with_normal10/$SCENE/
+        --dist_lambda 0 \
+        --result_dir results/3dgs_with_normal/$SCENE/
 
     # python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
     #     --model_type 2dgs_inria \
--- examples/simple_trainer_mcmc.py
+++ examples/simple_trainer_mcmc.py
@@ -477,6 +477,7 @@ class Runner:
                     near_plane=cfg.near_plane,
                     far_plane=cfg.far_plane,
                 )
+                normals_surf = normals_surf * (alphas).detach()
                 normalconsistencyloss = (
                     1 - (normals * normals_surf).sum(dim=-1)
                 ).mean()
@@ -753,7 +754,7 @@ class Runner:
                 depths = renders[..., -1:]
                 depths = (depths - depths.min()) / (depths.max() - depths.min())
                 canvas_list.append(apply_float_colormap(1 - depths, colormap="turbo"))
-            if renders.shape[-1] >= 5: # cfg.normal_consistency_loss:
+            if renders.shape[-1] >= 5:  # cfg.normal_consistency_loss:
                 depths = renders[..., -1:]
                 normals = renders[..., -4:-1]
                 normals_surf = depth_to_normal(
@@ -763,6 +764,7 @@ class Runner:
                     near_plane=cfg.near_plane,
                     far_plane=cfg.far_plane,
                 )
+                normals_surf = normals_surf * (alphas).detach()
                 canvas_list.extend([normals * 0.5 + 0.5])
                 canvas_list.extend([normals_surf * 0.5 + 0.5])
             if cfg.dist_loss:
@@ -855,7 +857,7 @@ class Runner:
                 depths = renders[..., -1:]
                 depths = (depths - depths.min()) / (depths.max() - depths.min())
                 canvas_list.append(apply_float_colormap(1 - depths, colormap="turbo"))
-            if renders.shape[-1] >= 5: # cfg.normal_consistency_loss:
+            if renders.shape[-1] >= 5:  # cfg.normal_consistency_loss:
                 depths = renders[..., -1:]
                 normals = renders[..., -4:-1]
                 normals_surf = depth_to_normal(
@@ -865,6 +867,7 @@ class Runner:
                     near_plane=cfg.near_plane,
                     far_plane=cfg.far_plane,
                 )
+                normals_surf = normals_surf * (alphas).detach()
                 canvas_list.extend([normals * 0.5 + 0.5])
                 canvas_list.extend([normals_surf * 0.5 + 0.5])
             if cfg.dist_loss:
