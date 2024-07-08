7b57f18866e783c83cd5608e16e4bb89eaaf1990
Jeffrey Hu <hujh14@gmail.com>
2024-07-07 22:51:52 -0700

    uncomment

--- examples/benchmark_mcmc.sh
+++ examples/benchmark_mcmc.sh
@@ -23,8 +23,18 @@ do
     echo "Running $SCENE"
     EVAL_STEPS="1000 7000 15000 30000"
 
-    for LAMBDA in 100 10 1 0;
+    for LAMBDA in 1 10 100;
     do
+        python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
+            --model_type 2dgs_inria \
+            --init_type sfm \
+            --cap_max $CAP_MAX \
+            --data_dir data/360_v2/$SCENE/ \
+            --normal_consistency_loss \
+            --dist_loss \
+            --dist_lambda $LAMBDA \
+            --result_dir results/2dgs_inria_with_normal/$SCENE/$LAMBDA/
+
         python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
             --model_type 3dgs \
             --init_type sfm \
@@ -34,37 +44,27 @@ do
             --dist_loss \
             --dist_lambda $LAMBDA \
             --result_dir results/3dgs_with_normal/$SCENE/$LAMBDA/
-
-        python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
-            --model_type 2dgs_inria \
-            --init_type sfm \
-            --cap_max $CAP_MAX \
-            --data_dir data/360_v2/$SCENE/ \
-            --normal_consistency_loss \
-            --dist_loss \
-            --dist_lambda $$LAMBDA \
-            --result_dir results/2dgs_inria_with_normal/$SCENE/$LAMBDA/
     done
 
-    # python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
-    #     --model_type 3dgs \
-    #     --init_type sfm \
-    #     --cap_max $CAP_MAX \
-    #     --data_dir data/360_v2/$SCENE/ \
-    #     --result_dir results/3dgs/$SCENE/
+    python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
+        --model_type 3dgs \
+        --init_type sfm \
+        --cap_max $CAP_MAX \
+        --data_dir data/360_v2/$SCENE/ \
+        --result_dir results/3dgs/$SCENE/
 
-    # python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
-    #     --model_type 2dgs_inria \
-    #     --init_type sfm \
-    #     --cap_max $CAP_MAX \
-    #     --data_dir data/360_v2/$SCENE/ \
-    #     --result_dir results/2dgs_inria/$SCENE/
+    python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
+        --model_type 2dgs_inria \
+        --init_type sfm \
+        --cap_max $CAP_MAX \
+        --data_dir data/360_v2/$SCENE/ \
+        --result_dir results/2dgs_inria/$SCENE/
 
-    # python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
-    #     --model_type 3dgs_inria \
-    #     --init_type sfm \
-    #     --cap_max $CAP_MAX \
-    #     --data_dir data/360_v2/$SCENE/ \
-    #     --result_dir results/3dgs_inria/$SCENE/
+    python simple_trainer_mcmc.py --eval_steps $EVAL_STEPS --disable_viewer --data_factor $DATA_FACTOR \
+        --model_type 3dgs_inria \
+        --init_type sfm \
+        --cap_max $CAP_MAX \
+        --data_dir data/360_v2/$SCENE/ \
+        --result_dir results/3dgs_inria/$SCENE/
 
 done
