4ac2345134fedd1f0524c494fb50c8f92e791267
Jeffrey Hu <hujh14@gmail.com>
2024-10-03 15:56:29 -0700

    app_opt

---
+++ examples/benchmarks/neural/app_opt.sh
@@ -0,0 +1,30 @@
+SCENE_DIR="data/360_v2"
+SCENE_LIST="garden bicycle stump bonsai counter kitchen room treehill flowers"
+RENDER_TRAJ_PATH="ellipse"
+
+# 1M GSs
+RESULT_DIR="results/benchmark_mcmc_1M_apt_opt"
+CAP_MAX=1000000
+
+# # 2M GSs
+# RESULT_DIR="results/benchmark_mcmc_2M_apt_opt"
+# CAP_MAX=2000000
+
+for SCENE in $SCENE_LIST;
+do
+    if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
+        DATA_FACTOR=2
+    else
+        DATA_FACTOR=4
+    fi
+
+    echo "Running $SCENE"
+
+    # train and eval
+    CUDA_VISIBLE_DEVICES=0 python simple_trainer.py mcmc --disable_viewer --data_factor $DATA_FACTOR \
+        --strategy.cap-max $CAP_MAX \
+        --render_traj_path $RENDER_TRAJ_PATH \
+        --app_opt \
+        --data_dir $SCENE_DIR/$SCENE/ \
+        --result_dir $RESULT_DIR/$SCENE/
+done
